4abd68f4bf28be18c50bc370103e4bd27096644b
torinhalvorson <torin.halvorson@gmail.com>
2023-11-20 23:41:30 -0800

    final updates

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+# Shiny App: Penguins Plots
 
+
+Link: https://torinhalvorson.shinyapps.io/assignment-b3-torinhalvorson/
+
+
+## Description:
+The goal of this project was to create a shiny app with three distinct features. This app uses the `penguins` dataset from the `palmerpenguins` package to create plots or summary tables of the data. The user specifies the x- and y-axes (labels: x-axis, y-axis), as well as the grouping colour (label 'Colour'), of the plot, and the table value (label: Table Value) and grouping variable (label: Table Grouping) for the table. The plot and the table are generated on two separate tabs for convenience. The app also includes an image of penguins to increase visual appeal.
+
+## Features:
+
+1. Plot with customizable input parameters
+  - The user can specify any variable from the `penguins` dataset on the x-axis, any numeric variable on the y-axis, and any categorical variable as colour. 
+  
+2. Table with customizable grouping and summary variables
+ - The user specifies one categorical variable from the `penguins` dataset to be used as a grouping variable, and a second numeric variable for which mean, standard deviation (sd) and count (n) are summarized in the table.
+ 
+3. Tabset layout
+ - The plot and table are generated on separate tabs, creating a clean layout
+ 
+4. Image
+ - A representative image of young penguins is included in the sidebar for visual appeal.
--- app.R
+++ app.R
@@ -1,14 +1,26 @@
+#load required packages
 library(shiny)
 library(palmerpenguins)
 library(ggplot2)
 library(dplyr)
 
+#define vectors to be used as input choices
 x_vector <- colnames(penguins)
 y_vector <- colnames(select(penguins, where(is.numeric)))
 colour_vector <- colnames(select(penguins, where(is.factor)))
 summary_vector <- colnames(select(penguins, where(is.factor)))
 
+#define user interface, including inputs to customize the plots and table
+#the plot and table will be shown as outputs on separate tabs
+#The four key features are created below: 
+# (1) Plot with customizable input parameters - this is useful for selectively and efficiently viewing relationships between variables of interest
+# (2) Table with customizable input parameters - this is useful for efficiently generating summary statistics of a desired variable ("Table Value") within specific groups (defined by "Table Variable")
+# (3) Tabset format - the table and plot are on separate tabs, allowing clean visualization of less overwhelming amounts of data
+# (4) Image - a representative image of penguins adds visual appeal to the interface
+
 ui <- fluidPage(
+
+  #Create the title panel and establish the sidebar layout format and sidebar panel
   titlePanel("Penguins Plots"),
   sidebarLayout(
     sidebarPanel(
@@ -17,8 +29,12 @@ ui <- fluidPage(
       selectInput("colour", label = "Colour", choices = colour_vector),
       selectInput("table_value", label = "Table Value", choices = y_vector),
       selectInput("table_group", label = "Table Grouping", choices = summary_vector),
+
+      #Add an image and customize its width and alignment to fit the page
       img(src = "penguins_img.png", width = "97%", align = "left")
   ), 
+  
+#Create main panel with separate tabs for the plot and the table
   mainPanel(
     tabsetPanel(
       tabPanel("Plot", plotOutput("plot1")),
@@ -28,6 +44,7 @@ ui <- fluidPage(
 )
 )
 
+#the server function creates a jitterplot and a table, both mapped to the ui input options
 server <- function(input, output, session) {
   output$plot1 <- renderPlot({
     ggplot(data = penguins, mapping = aes_string(x = input$xaxis, y = input$yaxis)) +
