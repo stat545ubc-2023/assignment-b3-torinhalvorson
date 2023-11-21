49c79ab14be52c29e1c9d3f615fd4c5971ed40c7
torinhalvorson <torin.halvorson@gmail.com>
2023-11-20 22:57:04 -0800

    updates

--- app.R
+++ app.R
@@ -1,24 +1,43 @@
 library(shiny)
 library(palmerpenguins)
 library(ggplot2)
+library(dplyr)
 
 x_vector <- colnames(penguins)
 y_vector <- colnames(select(penguins, where(is.numeric)))
 colour_vector <- colnames(select(penguins, where(is.factor)))
+summary_vector <- colnames(select(penguins, where(is.factor)))
 
 ui <- fluidPage(
-  selectInput("xaxis", label = "x-axis", choices = x_vector),
-  selectInput("yaxis", label = "y-axis", choices = y_vector),
-  selectInput("colour", label = "Colour", choices = colour_vector),
-  plotOutput("plot1")
+  titlePanel("Penguins Plots"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("xaxis", label = "x-axis", choices = x_vector), 
+      selectInput("yaxis", label = "y-axis", choices = y_vector), 
+      selectInput("colour", label = "Colour", choices = colour_vector),
+      selectInput("table_value", label = "Table Value", choices = y_vector),
+      selectInput("table_group", label = "Table Grouping", choices = summary_vector),
+      img(src = "penguins_img.png", width = "97%", align = "left")
+  ), 
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Plot", plotOutput("plot1")),
+      tabPanel("Summary Table", tableOutput("table1")),
+    )
+  )
+)
 )
 
-#need to fix colour output
 server <- function(input, output, session) {
   output$plot1 <- renderPlot({
     ggplot(data = penguins, mapping = aes_string(x = input$xaxis, y = input$yaxis)) +
-      geom_jitter(aes(colour = input$colour_vector), alpha = 0.5) +
+      geom_jitter(aes_string(colour = input$colour), alpha = 0.5) +
       theme_minimal()
+  })
+  output$table1 <- renderTable({
+    penguins %>%
+      group_by(!!sym(input$table_group)) %>%
+      summarize(mean = mean(!!sym(input$table_value), na.rm = TRUE), sd = sd(!!sym(input$table_value), na.rm = TRUE), n = n())
   })
 }
 
